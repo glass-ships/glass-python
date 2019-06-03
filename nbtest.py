13093bb87b16d06a9af3b042749103bbcae5a868
glass-ships <glass.ships@outlook.com>
2019-06-03 05:36:11 +0000

    Update nbtest.py

--- nbtest.py
+++ nbtest.py
@@ -1,13 +1,10 @@
+# notebook_runner.py
+ 
 import nbformat
 import os
 import sys
 
 from nbconvert.preprocessors import ExecutePreprocessor
-
-if os.path.exists(sys.argv[1]):
-    notebook_path = os.path.abspath(sys.argv[1])
-else:
-    print("Cannot find notebook \'",sys.argv[1],"\'")
     
 def run_notebook(notebook_path):
     nb_name, _ = os.path.splitext(os.path.basename(notebook_path))
@@ -32,8 +29,30 @@ def run_notebook(notebook_path):
                     errors.append(output)
  
     return nb, errors
- 
-if __name__ == '__main__':
-    nb, errors = run_notebook(notebook_path)
-    print("notebook errors: ",errors)
-    
+
+# check if argument is file or directory
+fp = os.path.abspath(sys.argv[1])
+if os.path.isfile(fp):
+    mode = 'single'
+    notebook_path = fp
+    print("notebook path: ",notebook_path)
+elif os.path.isdir(fp):
+    mode = 'multiple'
+    notebook_path = []
+    for file in os.listdir(fp):
+        if file.endswith(".ipynb"):
+            notebook_path.append(os.path.join(fp,file))
+    #print('notebooks: ',*notebook_path, sep = '\n')
+else:
+    print("Could not find notebooks at \'",sys.argv[1],"\'")    
+
+# test notebook; if directory, test all notebooks     
+if mode == 'single':
+    if __name__ == '__main__':
+        nb, errors = run_notebook(notebook_path)
+        print("notebook errors: ",errors)
+elif mode == 'multiple': 
+    for i in notebook_path:
+        if __name__ == '__main__':
+            nb, errors = run_notebook(i)
+            print("notebook: ",i,"\n errors: ",errors)
